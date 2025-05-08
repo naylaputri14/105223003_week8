423a45af6e38589eb9f6ee24ee0dbc2f733c67ea
naylaputri14 <nylaaptr@gmail.com>
2025-05-08 16:33:54 +0700

    Add files via upload

---
+++ 105223003_modul8/105223003_postTest7/src/no1/Main.java
@@ -0,0 +1,21 @@
+package no1;
+
+class Bangunan {
+    String nama = "Bangunan Umum";
+}
+
+class GedungSekolah extends Bangunan {
+    String nama = "Gedung Sekolah ABC";
+
+    void tampilkanInfo() {
+        System.out.println("Nama milik subclass " + nama);
+        System.out.println("Nama milik superclass " + super.nama);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        GedungSekolah gedung = new GedungSekolah();
+        gedung.tampilkanInfo();
+    }
+}
