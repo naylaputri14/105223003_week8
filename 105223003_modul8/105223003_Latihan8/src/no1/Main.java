18d5f5978a2d3d03a54cfe4759ee9ba7953972b1
naylaputri14 <nylaaptr@gmail.com>
2025-05-08 16:05:36 +0700

    Add files via upload

---
+++ 105223003_modul8/105223003_Latihan8/src/no1/Main.java
@@ -0,0 +1,21 @@
+package no1;
+
+class Hewan {
+    String suara = "Suara hewan";
+}
+
+class Kucing extends Hewan {
+    String suara = "Meong";
+
+    void tampilkanSuara() {
+        System.out.println("suara milik kelas Kucing " + suara);
+        System.out.println("suara milik kelas Hewan " + super.suara);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Kucing kucing = new Kucing();
+        kucing.tampilkanSuara();
+    }
+}
---
+++ 105223003_modul8/105223003_Latihan8/src/no2/Main.java
@@ -0,0 +1,20 @@
+package no2;
+
+class Kendaraan {
+    Kendaraan() {
+        System.out.println("Konstruktor Kendaraan dipanggil");
+    }
+}
+
+class Mobil extends Kendaraan {
+    Mobil() {
+        super();
+        System.out.println("Konstruktor Mobil dipanggil");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Mobil mobil = new Mobil();
+    }
+}
